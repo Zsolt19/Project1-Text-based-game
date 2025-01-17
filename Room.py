426121b23e9040c1e8630dbf58e814f7c61207e5
Zsolt19 <zsolt@levellada.net>
2025-01-17 11:29:34 +0000

    UPD set get/set for name

--- Room.py
+++ Room.py
@@ -1,7 +1,7 @@
 class Room:
     #constructor
-    def __init__ (self, room_name):
-        self.name=room_name
+    def __init__ (self):
+        self.name=None
         self.description=None
     #getter
     def get_description(self):
@@ -9,3 +9,12 @@ class Room:
     #setter
     def set_description(self, room_description):
         self.description=room_description   
+    
+    def describe(self):
+        print(self.description)
+    
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, room_name):
+        self.name=room_name
