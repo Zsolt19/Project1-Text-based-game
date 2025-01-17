3c06d0e25a952ae2070ba14c2ff83e260ebde1a5
Zsolt19 <zsolt@levellada.net>
2025-01-17 10:55:36 +0000

    UPD Crearte room class and set up rooms

---
+++ main.py
@@ -0,0 +1,11 @@
+from room import Room
+
+#set rooms
+kitchen=Room("Kitchen")
+ballroom=Room("Ballroom")
+dining_hall=Room("Dining Hall")
+
+#set descriptions
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+ballroom.set_description("A vast room with a shiny wooden floor; huge candlesticks guard the entrance.")
+dining_hall.set_description("A large room with ornate golden decorations on each wall.")
